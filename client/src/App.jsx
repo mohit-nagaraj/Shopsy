3c457cab8f4e30e146e825c25c8ab59b694ad4b5
ednamode-yoda <mohitnagaraj20@gmail.com>
2023-11-11 21:25:45 +0530

    Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,58 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet, //Our navbar n footer need not be added to every single component separately instead use this
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+import Error from "./pages/Error/Error";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+//creating roots
+// A <BrowserRouter> stores the current location in the browser's address bar using clean URLs and navigates using the browser's built-in history stack.
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,//thus now there a base layout, then the content changes according to the child routes
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+      //for 404 page (any undefined route)
+      {
+        path: "*",
+        element: <Error />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div>
+      {/* RouterProvider provides routes  */}
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      NavBar
+    </div>
+  )
+}
+
+export default Navbar
