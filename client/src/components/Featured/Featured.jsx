2b03091a44b4492abac7d5e8752f60078bd4295e
ednamode-yoda <mohitnagaraj20@gmail.com>
2023-11-20 21:22:13 +0530

    api testing

--- client/src/components/Featured/Featured.jsx
+++ client/src/components/Featured/Featured.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./featured.scss";
 import Card from "../Card/Card";
+import axios from "axios";
+
 const data = [
   {
     id: 'klanwd19w',
@@ -32,6 +34,23 @@ const data = [
 ];
 const Featured = ({ type }) => {
   const customStyle={ marginBottom: `${type == "Featured"?70:20}px`};
+  const [products, setProducts] = useState([]);
+  //can also use react query but we are not gonna use any post request so we can use useEffect
+  useEffect(()=>{
+    const fetchData = async() =>{
+      try{
+        const res= await axios.get("http://localhost:1337/api/products",{//header
+          headers:{
+            Authorization: "Bearer 6c5be87916f227fe8c5b47cc1d28c0624eb83b2b9cab2ecc9d97ddc15ceb5144a103fe586374bfff3dd6df92db9b07b02f3fe18d0ec7ab550b0d6f5607b6aa1b1a88fe95ffce6245ad86798942ad637448f35ff414be71eb4c8099bc6b802658dd59785fd25e456c26e4011837fb274c0f17a97b70081e7bff9f21449b6c8325",
+          },
+        });
+        console.log(res.data);
+      }catch(err){
+        console.log(err);
+      }
+    };
+    fetchData();
+  },[])
   return (
     <div className="featured" style={customStyle}>
       <div className="top">
